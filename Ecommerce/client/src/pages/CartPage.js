fdd273d840d9c37213f30b45c771cc6e8529eb02
KalpanadeviGunasekaran <128279103+KalpanadeviGunasekaran@users.noreply.github.com>
2024-04-27 23:21:01 +0530

    Update CartPage.js

--- Ecommerce/client/src/pages/CartPage.js
+++ Ecommerce/client/src/pages/CartPage.js
@@ -51,7 +51,7 @@ const CartPage = () => {
     try {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
-      const { data } = await axios.post("/api/v1/product/braintree/payment", {
+      const { data } = await axios.post("https://ecommercetextile-5.onrender.com/api/v1/product/braintree/payment", {
         nonce,
         cart,
       });
@@ -91,7 +91,7 @@ const CartPage = () => {
                 <div className="row card flex-row" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`/api/v1/product/product-photo/${p._id}`}
+                      src={`https://ecommercetextile-5.onrender.com/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
